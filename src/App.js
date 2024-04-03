fa45e3f299b9d391c0718bcc4d59641f733bc7d0
hojoon0724 <hojoon0724@gmail.com>
2024-04-02 18:28:11 -0700

    removed unused component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import Compositions from './pages/compositions';
 
 function App() {
   return (
     <div className="App">
-      {/* <Compositions /> */}
       <Outlet />
     </div>
   );
